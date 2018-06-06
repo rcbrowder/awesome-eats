95f6cd7d9139cd18692a39acef7538f34220cba3
Chris Browder <rcbrowder@gmail.com>
2018-06-06 10:43:24 -0400

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,34 @@
+// Menu section
+var menu = document.getElementById('menu');
+
+// api variables
+var apiRequest;
+var url = 'http://entree-s18.herokuapp.com/v1/menu';
+
+function getMenu() {
+    apiRequest = new XMLHttpRequest();
+    apiRequest.onload = displayMenu;
+    apiRequest.open('get', url, true);
+    apiRequest.send();
+}
+
+function displayMenu() {
+    if (apiRequest.statusText == 'OK') {
+        var response = JSON.parse(apiRequest.responseText);
+        console.log(response.menu_items);
+        // Print description of each menu item on a new line
+        for (var item in response.menu_items) {
+            var node = document.createElement('li');
+            var menuArray = response.menu_items[item].description;
+            var textnode = document.createTextNode(menuArray);
+            console.log(textnode);
+            node.appendChild(textnode);
+            menu.appendChild(node);
+        }
+    }
+}
+getMenu();
+
+
+// console.log(response.menu_items[0].description);
+// console.log(response.menu_items[1]);
---
+++ index.js
@@ -0,0 +1,11 @@
+// Set up express server
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/app'));
+
+app.listen(port, function() {
+    console.log('I am listening on port ' + port);
+});
